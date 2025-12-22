9252bc87245eba91da6b950a286db5f6acf7c8bd
katelyn_x3 <samson.ne.bob@gmail.com>
2025-12-22 10:01:26 +0300

    тоже не проверила

---
+++ 7.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 30, 40, 50]
+search = int(input('Введите число для поиска '))
+index = -1
+for i in range(len(numbers)):
+    if numbers[i] == search:
+        index = i
+        print(f'Число найдено на позиции {i}')
+        break
+else:
+    print('Нет такого числа')
