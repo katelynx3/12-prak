6bac3c92e20e8cff8f871d08cc3d39ce8239081b
katelyn_x3 <samson.ne.bob@gmail.com>
2025-12-22 10:56:05 +0300

    рабочий !!!!!!!!!!!!!!! прошлый не работает

--- 555.py
+++ 555.py
@@ -1,1 +1,9 @@
-
+data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+one = data[:3]
+two = data[-3:]
+reverse = data[::-1]
+nechet = data[0::2]
+print(f'Первая тройка чисел: {one}')
+print(f'Последняя тройка чисел: {two}')
+print(f'Список в обратном порядке: {reverse}')
+print(f'Числа с нечетным индексом: {nechet}')
