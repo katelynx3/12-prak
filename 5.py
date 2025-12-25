ebbbb43c661747519842736e51cc9cb2ba790e7d
katelyn_x3 <samson.ne.bob@gmail.com>
2025-12-25 13:01:17 +0300

    Delete 5.py

--- 5.py
+++
@@ -1,9 +0,0 @@
-data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-one = data[:3]
-two = data[:-3]
-reverse = data[::-1]
-nechet = data[1::2]
-print(f'Первая тройка чисел: {one}')
-print(f'Последняя тройка чисел: {two}')
-print(f'Список в обратном порядке: {reverse}')
-print(f'Числа с нечетным индексом: {nechet}')
